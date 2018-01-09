a7175c79d9ee0c65b790975486d06b1ea5a21002
kairi1227 <kairi1227@126.com>
2018-01-09 13:24:06 +0800

    update configuration

---
+++ src/RotateMenu.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class RotateMenu extends React.PureComponent {
+
+  constructor() {
+    super(...arguments);
+  }
+
+  render() {
+    return (
+      <div>
+        this is a rotate menu!
+      </div>
+    );
+  }
+}
+
+export default RotateMenu;
