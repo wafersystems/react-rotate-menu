3bd89075b01d6edbce62c19e4b074f63be3d6091
kairi1227 <kairi1227@126.com>
2018-01-09 10:46:10 +0800

    initial project

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+'use strict';
+
+let path = require('path');
+
+console.log(path.join(__dirname, './src'));
+
+module.exports = {
+  entry: {
+	  RotateMenu: './src/RotateMenu.jsx'
+  },
+  output: {
+    path: path.join(__dirname, './dist'),
+    filename: '[name].js',
+    libraryTarget: "umd"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+        include: [path.join(__dirname, './src')]
+      }
+    ]
+  },
+  externals: {
+    'react': 'react'
+  }
+};
