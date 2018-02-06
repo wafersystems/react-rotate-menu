63d247d7df1a435474f7a454142093bee2165762
vkingw <alfa.king+git@gmail.com>
2018-02-06 09:50:02 +0800

    Test Example

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './RotateMenu.jsx';
+import './index.css'
 import registerServiceWorker from './registerServiceWorker';
 
 let data = []
